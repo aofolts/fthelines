350017b85397599a1438ddaa37fe7d98fb150d69
Andrew Folts <andrew@sherpadesign.co>
2019-10-17 14:38:20 -0400

    Fixed preview token

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,15 @@ try {
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || contentfulConfig.accessToken,
+  environment: contentfulConfig.environment || 'master'
 }
 
-const { spaceId, accessToken } = contentfulConfig
+const { 
+  spaceId, 
+  accessToken,
+  environment
+} = contentfulConfig
 
 if (!spaceId || !accessToken) {
   throw new Error(
@@ -38,14 +43,11 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-transformer-remark`,
     {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig
-    },
-    {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: spaceId,
-        accessToken: accessToken,
+        spaceId,
+        accessToken,
+        environment,
         host: `preview.contentful.com`,
       },
     },
